bb8d79f8d2e099b03550224d86b0fa633ee528c7
Dikshit <122359979+DIKSHITS@users.noreply.github.com>
2024-11-25 14:10:30 +0530

    Update Login.js

--- frontend/src/views/Login.js
+++ frontend/src/views/Login.js
@@ -23,7 +23,7 @@ function Login() {
     }
 
     try {
-      const response = await axios.post("http://localhost:3001/login", {
+      const response = await axios.post("https://ed-system.onrender.com/login", {
         email,
         password,
         category,
