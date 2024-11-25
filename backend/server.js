16d25813c912642ebe637e1b42affa03cd086024
Dikshit <122359979+DIKSHITS@users.noreply.github.com>
2024-11-25 15:18:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const port = 3001;
 app.use(express.json());
 app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://ed-system.onrender.com',
   credentials: true
 }));
 // Initialize express-session
